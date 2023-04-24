185d6e94547ced0ed57b154133a8d705b6179b28
root <root@ip-172-31-67-75.ec2.internal>
2023-04-24 09:02:59 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "arvind1973"
 
     workspaces {
       name = "learn-terraform-github-actions"
